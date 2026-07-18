df30950bfd652220e0e783c742cb6387566a5ac2
ghizlanennhaili-cloud <ghizlanennhaili@gmail.com>
2026-07-18 14:52:28 +0100

    Mise à jour du design

--- Script.js
+++ Script.js
@@ -23,62 +23,65 @@ document.addEventListener("DOMContentLoaded", async () => {
     const write = (text) => { output.innerText += text + "\n"; };
 
     write("> Initializing Love System...\n");
-    await sleep(1000);
+    await sleep(2000);
     write("Loading memories... 100%\n");
-    await sleep(1000);
+    await sleep(2000);
     write("Searching for the man who owns Youssra's heart...\n");
-    await sleep(1000);
+    await sleep(2000);
     write("Result found.\n");
-    await sleep(1000);
+    await sleep(2000);
 
     write("\nName    : Simo");
     write("Role    : Husband ❤️\n");
-    await sleep(1000);
+    await sleep(2000);
 
     write("Achievements unlocked:");
-    await sleep(500);
+     await sleep(1000);
     write("✔️ Occasionally tests Youssra's patience...");
-    await sleep(500);
+     await sleep(1000);
     write("✔️ He knows exactly how to make Youssra smile, and somehow, he also knows exactly how to make her mad 😒");
-    await sleep(500);
+     await sleep(1000);
     write("✔️ Sometimes makes Youssra upset...");
-    await sleep(1500);
+    await sleep(3000);
     write("\n   Just kidding 😈❤️\n");
-    await sleep(500);
+    await sleep(2000);
 
     write("✔ Stole Youssra's heart.");
-    await sleep(500);
+     await sleep(1000);
     write("✔ Became her best friend.");
-    await sleep(500);
+     await sleep(1000);
     write("✔ Became her safe place.");
-    await sleep(500);
+     await sleep(1000);
     write("✔ Became her husband.");
-    await sleep(500);
+     await sleep(1000);
     write("✔ Made her believe in forever.\n");
-    await sleep(1000);
+    await sleep(3000);
 
     write("Current mission:");
-    await sleep(500);
+     await sleep(1000);
     write("Love Youssra.");
-    await sleep(500);
+     await sleep(1000);
     write("Protect Youssra.");
-    await sleep(500);
+     await sleep(1000);
     write("Support Youssra.");
-    await sleep(500);
+     await sleep(1000);
     write("Make Youssra smile.");
-    await sleep(500);
+     await sleep(1000);
     write("Grow old with Youssra.\n");
-    await sleep(1000);
+    await sleep(2000);
 
     write("Mission duration:");
+     await sleep(1000);
     write("∞ Forever\n");
-    await sleep(1000);
+    await sleep(3000);
 
     write("No matter how many kilometers separate us,");
+     await sleep(1000);
     write("my heart will always choose you.\n");
-    await sleep(1000);
+    await sleep(3000);
 
     write("Happy First Wedding Anniversary, my love.\n");
+
     
     const love = document.createElement('div');
     love.className = 'final-love';
